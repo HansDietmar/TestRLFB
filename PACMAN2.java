bdc0fc2cd89f2f92089590b7196b005ce0e0c161
Peter <peter.brichzin@gymnasium-ottobrunn.de>
2020-02-27 11:48:38 +0100

    Eine neue Klasse PACMAN 2 ergänzt

---
+++ PACMAN2.java
@@ -0,0 +1,46 @@
+
+/**
+ * Beschreiben Sie hier die Klasse PACMAN.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class PACMAN2 extends SPIELFIGUR
+{
+    // Attribute 
+    boolean fressbar;
+
+    /**
+     * Konstruktor für Objekte der Klasse PACMAN
+     */
+    PACMAN2()
+    {
+        super(5, 5, 5); 
+        
+        fressbar = false;
+    }
+
+        /**
+    /**
+     * Methode zur Rueckgabe einer Charakterbeschreibung
+     * Implementierung der abstrakten Methode der Oberklasse
+     * 
+     * @return Charakterbeschreibung 
+     */
+    public String CharakterbeschreibungAusgeben()
+    {
+        return "Pacman wins!";
+        
+    }
+    
+    /**
+     * Gibt die Spielereigenschaften auf dem Ausgabefenster aus.
+     * Überschreibt die Methode der Oberklasse
+     */
+    public void SpielerEigenschaftenAusgeben()
+    {
+        System.out.println("fressbar :"+ fressbar);
+        // Aufruf der gleichnamigen Methode der Oberklasse
+        super.SpielerEigenschaftenAusgeben();
+    }
+}
